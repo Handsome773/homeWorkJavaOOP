420baa2d6cd72aed8b968adf16734bc31a88e34c
Nikita <773737@mail.ru>
2023-07-08 20:56:18 +0300

    DZ1

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         //Создаем объекты
-        Human father = new Human("Антон", LocalDate.of(2000, 10, 16), Gender.Male);
-        Human mother = new Human("Елена", LocalDate.of(2003, 04, 01), Gender.Female);
-        Human son = new Human("Андрей", LocalDate.of(2020, 12, 31), Gender.Male);
+        Human father = new Human("Александр", LocalDate.of(1996, 07, 11), Gender.Male);
+        Human mother = new Human("Ирина", LocalDate.of(2001, 05, 04), Gender.Female);
+        Human son = new Human("Никита", LocalDate.of(2020, 11, 23), Gender.Male);
 
         //Устанавливаем связи
         father.setChild(son);
@@ -16,6 +16,6 @@ public class Main {
         tree.addHuman(father);
         tree.addHuman(mother);
         tree.addHuman(son);
-        System.out.println(String.format("Состав семьи: %s", tree.toString()));
+        System.out.println(String.format("Семья: %s", tree.toString()));
     }
 }
